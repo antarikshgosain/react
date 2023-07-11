02f9972985d7ea3ae2ea3f058e25096407c3790d
antariksh39 <gosain.antariksh@gmail.com>
2023-07-12 04:36:35 +0530

    guess-num gradient over bg image added

--- guess-number-game/App.js
+++ guess-number-game/App.js
@@ -1,11 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <LinearGradient colors={['#ddb52f', '#4e0329']} style={styles.rootScreeen}>
-      <StartGameScreen/>
+      <ImageBackground source={require('./assets/images/bg-img-dice.jpg')} 
+        resizeMode="cover" style={styles.rootScreeen} imageStyle={styles.backgroundImage} >
+          
+        <StartGameScreen/>
+        </ImageBackground>
     </LinearGradient>
   );
 }
@@ -20,5 +24,8 @@ const styles = StyleSheet.create({
   rootScreeen: {
     flex: 1,
     backgroundColor: '#ddb52f',
+  }, 
+  backgroundImage : {
+    opacity: 0.9,
   }
 });
--- guess-number-game/screens/StartGameScreen.js
+++ guess-number-game/screens/StartGameScreen.js
@@ -2,22 +2,21 @@ import { StyleSheet, TextInput , View } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 function StartGameScreen(){
-
-    return  <View style={styles.inputContainer}>
-                <TextInput 
-                    style={styles.numberInput} maxLength={2} 
-                    keyboardType={'number-pad'} numberOfLines={1}
-                    />
-                <View style={styles.buttonsContainer}>
-                    <View style={styles.buttonContainer}>
-                        <PrimaryButton>Reset</PrimaryButton>
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <PrimaryButton>Confirm</PrimaryButton>
-                    </View>
-                </View>
+    return  
+    <View style={styles.inputContainer}>
+        <TextInput 
+            style={styles.numberInput} maxLength={2} 
+            keyboardType={'number-pad'} numberOfLines={1}
+        />
+        <View style={styles.buttonsContainer}>
+            <View style={styles.buttonContainer}>
+                <PrimaryButton>Reset</PrimaryButton>
             </View>
-
+            <View style={styles.buttonContainer}>
+                <PrimaryButton>Confirm</PrimaryButton>
+            </View>
+        </View>
+    </View>
 }
 
 export default StartGameScreen;
